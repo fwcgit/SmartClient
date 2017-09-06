6d411001f4673d2c9b855d01f7f5c5b1ff0eb90f
fwcgic <fu_huaicheng@163.com>
2017-09-06 21:37:57 +0800

    指令编写

--- app/src/main/cpp/Socket.cpp
+++ app/src/main/cpp/Socket.cpp
@@ -314,7 +314,7 @@ JNIEXPORT void JNICALL Java_com_fu_smart_jni_Socket_openPower
 
     char *data = (char*)malloc(5* sizeof(char));
 
-    *(data) = 0x3a;
+    *(data) = 0x3b;
     *(data+1) = 0x4a;
     *(data+2) = 0x53;
     *(data+3) = (cmd == 1 ? 0x01:0x02);
